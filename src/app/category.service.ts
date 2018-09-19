ea756198412cd7b741e66696a8b2e72073e6d663
Ammar <a.b.ameerdeen@gmail.com>
2018-09-19 18:58:16 +0100

    categories information transfered to .json file in assets folder.

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,30 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Category } from './category/category.model';
-import {Http} from  '@angular/http';
-import { Observable } from 'rxjs';
+import * as categoriesS from '../assets/categories.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  categories : Category[] = [
-      { categoryName :"Men’s Clothing",level :1, categoryId : 1, imageUrl : '/assets/category_images/men.bmp' , subCategories : [
-        { categoryName :"Fredd Marshal" ,level :2, categoryId :1 , imageUrl : '/assets/category_images/men/fredd.bmp' , subCategories : null} ,
-        { categoryName :"Inflation" ,level :2, categoryId :1 , imageUrl : '/assets/category_images/men/inflation.bmp' , subCategories : null} ,
-        { categoryName :"Nersy" ,level :2, categoryId :1 , imageUrl : '/assets/category_images/men/nersy.bmp' , subCategories : null} ]},
-      { categoryName :"Women’s Clothing",level :1, categoryId : 1, imageUrl : '/assets/category_images/women.bmp' , subCategories : [
-        { categoryName :"Love & Lemonade",level :2, categoryId : 1, imageUrl : '/assets/category_images/women/love.bmp' , subCategories : null},
-        { categoryName :"Valdim",level :2, categoryId : 1, imageUrl : '/assets/category_images/women/valdim.bmp' , subCategories : null},
-        { categoryName :"Simplee",level :2, categoryId : 1, imageUrl : '/assets/category_images/women/simplee.bmp' , subCategories : null}
-      ]},
-      { categoryName :"Cellphones & Accessories",level :1, categoryId : 1, imageUrl : '/assets/category_images/cellphones.bmp' , subCategories : null},
-      { categoryName :"Computer, Office, Security",level :1, categoryId : 1, imageUrl : '/assets/category_images/computer.bmp' , subCategories : null},
-      { categoryName :"Jewelry & Watches",level :1, categoryId : 1, imageUrl : '/assets/category_images/jewellery.bmp' , subCategories : null},
-      { categoryName :"Bags & Shoes",level :1, categoryId : 1, imageUrl : '/assets/category_images/bags.bmp' , subCategories : null},
-      { categoryName :"Toys, Kids & Baby",level :1, categoryId : 1, imageUrl : '/assets/category_images/toys.bmp' , subCategories : null},
-      { categoryName :"Sport & Outdoors",level :1, categoryId : 1, imageUrl : '/assets/category_images/sports.bmp' , subCategories : null}
-    ];
+  categories : Category[] = categoriesS.default;
 
   public selectedCategories : Category[] = [];
   constructor() { 
